77806500898ea396a20402344170ba61b1bb1750
Kabir Jaiswal <kabirjaiswal30@gmail.com>
2023-05-10 04:26:37 -0400

    kdb_operator Sphinx

--- airflow_kdb_provider/operators/kdb_operator.py
+++ airflow_kdb_provider/operators/kdb_operator.py
@@ -2,7 +2,20 @@ from airflow.models import BaseOperator
 from qpython import qconnection
 
 class KdbOperator(BaseOperator):
+    """
+    Executes a KDB query and returns the result.
 
+    This operator is designed to integrate KDB, a high-performance column-store database, into Apache Airflow pipelines. It allows users to connect to a KDB instance, execute a query, and handle the results within a DAG.
+    
+    Note: This operator assumes that the necessary KDB libraries and authentication methods are properly configured in the Airflow environment.
+
+    :param kdb_conn_id: The KDB connection ID to use for connecting to the KDB instance.
+    :type kdb_conn_id: str
+    :param query: The KDB query to execute.
+    :type query: str
+    :param parameters: (Optional) A dictionary of query parameters to be passed to the KDB query.
+    :type parameters: dict
+    """
     def __init__(
             self,
             command,
